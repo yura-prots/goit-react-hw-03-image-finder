6e4d81df9931be0c860448bdd5dd30e4727bc3dc
yura-prots <yuraprots@gmail.com>
2024-01-28 23:40:54 +0200

    add modal

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,13 +1,55 @@
 import { Component } from 'react';
+import Modal from 'react-modal';
+
 import { GalleryItem, GalleryImage } from './ImageGalleryItem.styled';
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement('#root');
+
 class ImageGalleryItem extends Component {
+  state = {
+    isModalOpen: false,
+  };
+
+  openModal = () => {
+    this.setState({
+      isModalOpen: true,
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      isModalOpen: false,
+    });
+  };
+
   render() {
+    const { isModalOpen } = this.state;
     const { webformatURL, tags } = this.props.imageSource;
 
     return (
-      <GalleryItem>
+      <GalleryItem onClick={this.openModal}>
         <GalleryImage src={webformatURL} alt={tags} />
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Gallery Image Modal"
+        >
+          <button type="button" onClick={this.closeModal}>
+            close
+          </button>
+        </Modal>
       </GalleryItem>
     );
   }
